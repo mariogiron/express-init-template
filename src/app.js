d1bd93a3a33d77586214af5b6d26f3d03f1dcd53
Mario Girón <mariogironm@gmail.com>
2023-11-27 18:46:21 +0100

    cors included

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 // Creación y configuración de la APP de Express
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Configuración de rutas
 
