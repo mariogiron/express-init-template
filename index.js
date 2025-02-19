454be14dbdc8895c2372599e7ddb11cb16ece3c6
Mario Girón <mariogironm@gmail.com>
2025-02-19 13:13:14 +0100

    BIome integration

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Server creation and configuration
-const http = require('http');
-const app = require('./src/app');
+const http = require("node:http");
+const app = require("./src/app");
 
 // Config .env
-require('dotenv').config();
+require("dotenv").config();
 
 // Server creation
 const server = http.createServer(app);
@@ -12,10 +12,16 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT);
 
 // Listeners
-server.on('listening', () => {
+server.on("listening", () => {
     console.log(`Server listening on port ${PORT}`);
 });
 
-server.on('error', (error) => {
+server.on("error", (error) => {
     console.log(error);
 });
+
+function sumar(a, b) {
+    return a + b;
+}
+
+sumar();
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 // Creation and configuration of the Express APP
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
@@ -8,19 +8,19 @@ app.use(cors());
 
 // Route configuration
 // Ex.
-// app.use('/api', require('./routes/api'));
+// app.use('/api', require('./routes/api.routes'));
 
 // 404 handler
 app.use((req, res, next) => {
     res.status(404).json({
-        message: 'Not found'
+        message: "Not found"
     });
-})
+});
 
 // Error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack)
+    console.error(err.stack);
     res.status(500).json({ message: err.message });
-})
+});
 
 module.exports = app;
